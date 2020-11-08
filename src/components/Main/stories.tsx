7c2c3edcc38aebad23e2676e26365c931d659d0d
Mario Rodeghiero <mario.rodeghiero@gmail.com>
2020-11-08 15:33:32 -0300

    [storybook][s]: set up storybook

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,13 +1,12 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Main from '.'
 
 export default {
 	title: 'Main',
 	component: Main,
-	decorators: [withKnobs]
+	args: {
+		title: 'ReactJS'
+	}
 } as Meta
 
-export const Basic: Story = () => (
-	<Main title={text('Title', 'React Avancado')} />
-)
+export const Basic: Story = (args) => <Main {...args} />
